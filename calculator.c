6b7158c5b2b283bdd96ddae11506ff663f0440a1
Parham <parhamfaizolahi@gmail.com>
2023-12-05 21:56:52 +0330

    Committing changes before switching branches

--- calculator.c
+++ calculator.c
@@ -1,1 +1,6 @@
-#include <stdio.h>\n\nint calculate(int a, int b) {\n  //A different calculation\n  return a + b;\n}
+#include <stdio.h>
+
+int calculate(int a, int b) {
+  //A different calculation
+  return a + b;
+}
